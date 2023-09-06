c4a52049f995b73faf20e9869f7db4d7b2f199cf
Ivan <ivan.hf.leung@gmail.com>
2023-09-06 09:11:18 -0400

    updated

--- app/index.js
+++ app/index.js
@@ -1,3 +1,4 @@
+// === GET (POST) === //
 const endpoint = "https://expo-post-app-8d5ed-default-rtdb.firebaseio.com/";
 
 async function getPosts() {
@@ -11,5 +12,25 @@ async function getPosts() {
   console.log(postsArray);
 }
 
+// === CREATE (POST) === //
+async function createPost(title, image) {
+  const newPost = { title, image };
+  const postAsJson = JSON.stringify(newPost);
+
+  const response = await fetch(`${endpoint}/posts.json`, {
+    method: "POST",
+    body: postAsJson,
+  });
+
+  if (response.ok) {
+    // data created - update UI
+  }
+}
+
+// test the function
+createPost(
+  "My First Post",
+  "https://images.unsplash.com/photo-1641876749963-550554c7258d"
+);
 // test the function
 getPosts();
