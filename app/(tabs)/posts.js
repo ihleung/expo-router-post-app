ac906d915a019e8e1fd31a9c6c33df218142116f
Ivan <ivan.hf.leung@gmail.com>
2023-09-09 22:36:39 -0400

    updated

--- app/(tabs)/posts.js
+++ app/(tabs)/posts.js
@@ -1,27 +1,36 @@
+import React from "react";
 import { Stack, useRouter } from "expo-router";
 import { useEffect, useState } from "react";
+import { useFocusEffect } from "@react-navigation/native"; // Import the useFocusEffect hook
 import { Button, FlatList, Platform, StyleSheet, View } from "react-native";
 import PostListItem from "../components/PostListItem";
 
+async function getPosts(setPosts) {
+  const response = await fetch(
+    "https://expo-post-app-8d5ed-default-rtdb.firebaseio.com/posts.json"
+  );
+  const dataObj = await response.json();
+  const postsArray = Object.keys(dataObj).map((key) => ({
+    id: key,
+    ...dataObj[key],
+  })); // from object to array
+  postsArray.sort((postA, postB) => postB.createdAt - postA.createdAt);
+  setPosts(postsArray);
+}
 export default function Posts() {
   const router = useRouter();
   const [posts, setPosts] = useState([]);
 
   useEffect(() => {
-    async function getPosts() {
-      const response = await fetch(
-        "https://expo-post-app-8d5ed-default-rtdb.firebaseio.com/posts.json"
-      );
-      const dataObj = await response.json();
-      const postsArray = Object.keys(dataObj).map((key) => ({
-        id: key,
-        ...dataObj[key],
-      })); // from object to array
-      postsArray.sort((postA, postB) => postB.createdAt - postA.createdAt);
-      setPosts(postsArray);
-    }
-    getPosts();
+    getPosts(setPosts);
   }, []);
+
+  // Use the useFocusEffect hook to fetch posts when the screen is focused
+  useFocusEffect(
+    React.useCallback(() => {
+      getPosts(setPosts);
+    }, [])
+  );
 
   function showCreateModal() {
     router.push("/create");
