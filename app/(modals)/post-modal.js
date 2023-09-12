cafdda51a42671a559f035aec016fbaba4ef9079
Ivan <ivan.hf.leung@gmail.com>
2023-09-12 11:15:14 -0400

    updated

--- app/(modals)/post-modal.js
+++ app/(modals)/post-modal.js
@@ -121,7 +121,9 @@ export default function PostModal() {
         <Image
           style={styles.image}
           source={{
-            uri: "https://www.pulsecarshalton.co.uk/wp-content/uploads/2016/08/jk-placeholder-image.jpg",
+            uri:
+              image ||
+              "https://www.pulsecarshalton.co.uk/wp-content/uploads/2016/08/jk-placeholder-image.jpg",
           }}
         />
       </TouchableOpacity>
